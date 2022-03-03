8392a17891952cdf62c8d09155ff044a7b21f5f9
Tominator3Trillion <89489218+Tominator3Trillion@users.noreply.github.com>
2022-03-03 15:46:51 -0800

    bugs

---
+++ Assets/Scripts/Lobby/PanelPopup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelPopup : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void InvertActive()
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
---
+++ Assets/Scripts/Player/RotateCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate teh camera on the world y axis when q or e are pressed
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * -100, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 100, Space.World);
+        }
+    }
+}
